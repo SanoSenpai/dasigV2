c98c155cdaa3c22112e80ff6546036d5bcbc92f2
SeanSaligue <ssaligue@asi-dev1.com>
2024-03-20 14:39:43 +0800

    Fixed home module not asserted in the root app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
 import { routes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [RouterModule.forRoot(routes), SharedModule, BrowserModule],
+  imports: [
+    RouterModule.forRoot(routes),
+    SharedModule,
+    BrowserModule,
+    CommonModule,
+    HomeModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
