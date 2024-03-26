7ed3eafefd6dd39f359b4d15eded0543808fa66a
SeanSaligue <ssaligue@asi-dev1.com>
2024-03-26 13:33:04 +0800

    Added current section (that is on-screen) logic for the nav links (changes color when in view)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { LanguageService } from './shared/services/language.service';
 import { LanguageData } from './shared/services/language.interface';
 import { Subscription } from 'rxjs';
@@ -14,10 +20,12 @@ export class AppComponent implements OnInit, OnDestroy {
   sub!: Subscription;
   showSpinner!: boolean;
   isNavbarScrolled: boolean = false;
+  currentSection!: string;
 
   constructor(
     private _lang: LanguageService,
-    private _spinner: SpinnerService
+    private _spinner: SpinnerService,
+    private elementRef: ElementRef
   ) {
     _lang.currentLanguage = 'en-ph';
     this._spinner.showSpinner(true);
@@ -27,6 +35,30 @@ export class AppComponent implements OnInit, OnDestroy {
   onWindowScroll() {
     // Check if the user has scrolled down by checking the vertical scroll position
     this.isNavbarScrolled = window.scrollY > 0;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            // If the section is in view, set it as the current section
+            this.currentSection = entry.target.id;
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5,
+      }
+    );
+
+    // Get all sections
+    const sections = this.elementRef.nativeElement.querySelectorAll('.section');
+
+    // Observe each section
+    sections.forEach((section: Element) => {
+      observer.observe(section);
+    });
   }
 
   ngOnInit(): void {
