3e66face5310da06a95b5bd5db2813045d3203ce
SeanSaligue <ssaligue@asi-dev1.com>
2024-04-01 09:43:10 +0800

    Added nav modal for mobile

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SpinnerComponent } from './components/spinner.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NavModalComponent } from './components/nav-modal/nav-modal.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
-  declarations: [SpinnerComponent],
+  declarations: [SpinnerComponent, NavModalComponent],
   imports: [CommonModule, HttpClientModule, FormsModule],
-  exports: [SpinnerComponent, CommonModule],
+  exports: [SpinnerComponent, NavModalComponent, CommonModule],
 })
 export class SharedModule {}
